c6d03916c54f00c2aad2f888424e65cacf92bbef
Darko <67342682+Darkosoftware@users.noreply.github.com>
2021-06-30 13:28:13 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+
+def add():
+	account = input("Account: ")
+	username = input("Username: ")
+	password = input("Password: ")
+	with open('passwordstest.txt','a') as f:
+		f.write(account +"|" + username + "|" + password[::-1] + "\n")
+
+def view():
+	with open('passwordstest.txt', 'r') as f:
+		for line in f.readlines():
+			data = line.rstrip()
+			account, username, password = data.split("|")
+			print("Account: ", account, "Username: ", username, "Password: ", password[::-1])
+
+while True:
+	password_ui = input("Input Master Password: ")
+	if password_ui == "masterpass":
+		mode = input("Would you like to view existing passwords or add new ones (view,add, q to quit)?: ")
+		if mode == "q":
+			break
+		if mode == "view":
+			view()
+		elif mode == "add":
+			add()
+		else:
+			print("Invalid Entry.")
+			continue
+	else:
+		print("Incorrect Master Password.")
+		continue
