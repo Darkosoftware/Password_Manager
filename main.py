ca1af7d74b22dbfa47135c28d07fc48e7d92f03a
Darko <67342682+Darkosoftware@users.noreply.github.com>
2021-07-12 19:00:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,21 +14,36 @@ def view():
 			account, username, password = data.split("|")
 			print("Account: ", account, "Username: ", username, "Password: ", password[::-1])
 
-while True:
-	password_ui = input("Input Master Password: ")
-	if password_ui == "masterpass":
-		mode = input("Would you like to view existing passwords or add new ones (view,add, q to quit)?: ")
-		if mode == "q":
-			break
-		if mode == "view":
-			view()
-		elif mode == "add":
-			add()
-		else:
-			print("Invalid Entry.")
-			continue
-	else:
-		print("Incorrect Master Password.")
-		continue
+def scramble():
+	pass
 
-		
+def descramble():
+	pass
+
+def set_save_masterpassword():
+	global master_password
+	filesize = os.stat('masterpassword.txt').st_size
+	with open('masterpassword.txt', 'r') as f:
+		master_password = f.readline()
+	if filesize == 0:
+		master_password = input("Set master password for the Password Manager: ")
+		with open('masterpassword.txt','a') as f:
+			f.write(master_password[::-1])
+
+while True:
+    set_save_masterpassword()
+    password_ui = input("Input Master Password: ")
+    if password_ui == master_password[::-1]:
+    	mode = input("Would you like to view existing passwords or add new ones (view,add, q to quit)?: ")
+    	if mode == "q":
+    		break
+    	if mode == "view":
+    		view()
+    	elif mode == "add":
+    		add()
+    	else:
+    		print("Invalid Entry.")
+    		continue
+    else:
+    	print("Incorrect Master Password.")
+    	continue
